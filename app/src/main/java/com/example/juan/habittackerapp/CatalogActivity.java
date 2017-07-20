351c26195b607baae0ddc00b73d1b94193aeaa58
Juan <mangasverdes.8@gmail.com>
2017-07-20 19:36:54 +0200

    Defined allRoutinesCursor

--- app/src/main/java/com/example/juan/habittackerapp/CatalogActivity.java
+++ app/src/main/java/com/example/juan/habittackerapp/CatalogActivity.java
@@ -19,6 +19,28 @@ public class CatalogActivity extends AppCompatActivity {
 
     private RoutineDbHelper mDbHelper;
 
+    public Cursor queryAllRoutines() {
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        String[] projection = {
+                RoutineEntry._ID,
+                RoutineEntry.COLUMN_ROUTINE_DATE,
+                RoutineEntry.COLUMN_ROUTINE_EXERCISE,
+                RoutineEntry.COLUMN_ROUTINE_TYPE,
+                RoutineEntry.COLUMN_ROUTINE_TIME};
+
+        Cursor allRoutinesCursor = db.query(
+                RoutineEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        return allRoutinesCursor;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,27 +61,10 @@ public class CatalogActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        displayDatabaseInfo();
+        displayAllRoutinesInDB(queryAllRoutines());
     }
 
-    private void displayDatabaseInfo() {
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        String[] projection = {
-                RoutineEntry._ID,
-                RoutineEntry.COLUMN_ROUTINE_DATE,
-                RoutineEntry.COLUMN_ROUTINE_EXERCISE,
-                RoutineEntry.COLUMN_ROUTINE_TYPE,
-                RoutineEntry.COLUMN_ROUTINE_TIME};
-
-        Cursor cursor = db.query(
-                RoutineEntry.TABLE_NAME,
-                projection,
-                null,
-                null,
-                null,
-                null,
-                null);
+    private void displayAllRoutinesInDB(Cursor cursor) {
 
         TextView displayView = (TextView) findViewById(R.id.text_view_routine);
 
@@ -101,7 +106,7 @@ public class CatalogActivity extends AppCompatActivity {
         values.put(RoutineEntry.COLUMN_ROUTINE_EXERCISE, getString(R.string.dummy_exercise));
         values.put(RoutineEntry.COLUMN_ROUTINE_TYPE, RoutineEntry.TYPE_CARDIO_WO);
         values.put(RoutineEntry.COLUMN_ROUTINE_TIME, getString(R.string.dummy_time));
-        long newRowId = db.insert(RoutineEntry.TABLE_NAME, null, values);
+        db.insert(RoutineEntry.TABLE_NAME, null, values);
     }
 
     @Override
@@ -115,7 +120,7 @@ public class CatalogActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_insert_dummy_data:
                 insertRoutine();
-                displayDatabaseInfo();
+                displayAllRoutinesInDB(queryAllRoutines());
                 return true;
 
         }
